afd5d2c53eb1464b55674168b9709a31f49535b5
Maksim Narushevich <narushevich.maksim@gmail.com>
2018-05-19 14:27:58 +0300

    - Made basic Tasks CRUD application by implementing Repository Pattern - Separated logic in controller from retrieving data from DB - Made controllers more testable and easy readable

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,5 +12,4 @@
             </ul>
         @endif
     </div>
-
 @stop
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,26 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/posts','PostController');
+
+// Task Routes
+Route::group(['prefix' => 'tasks'], function () {
+    Route::get('/{id?}', [
+        'uses' => 'TaskController@getAllTasks',
+        'as' => 'task.index'
+    ]);
+
+    Route::post('store', [
+        'uses' => 'TaskController@postStoreTask',
+        'as' => 'task.store'
+    ]);
+
+    Route::patch('{id}/update', [
+        'uses' => 'TaskController@postUpdateTask',
+        'as' => 'task.update'
+    ]);
+
+    Route::delete('{id}/delete', [
+        'uses' => 'TaskController@postDeleteTask',
+        'as' => 'task.delete'
+    ]);
+});
