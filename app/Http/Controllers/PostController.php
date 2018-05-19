9ec91e53719a728b0bdc48dfd7ec42b2cddfe98a
Maksim Narushevich <narushevich.maksim@gmail.com>
2018-05-19 13:16:43 +0300

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+        $posts=Post::all();
+        $title='Posts';
+        return view('posts.index', compact('title','posts'));
+    }
+}
